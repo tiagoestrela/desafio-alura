f45c2159d0b53598a860d103d7c074da6ec7aa25
Tiago Estrela <56174000+tiagoestrela@users.noreply.github.com>
2025-01-20 14:47:28 -0300

    origin

---
+++ js-curso-2-aula_1/app.js
@@ -0,0 +1,22 @@
+let numeroSecreto = gerarNumeroAleatorio();
+
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número secreto de 1 a 10');
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+}
+
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
